70d188b2017add898b128428931c0e3004210bff
feci <ferenchegymegi3@gmail.com>
2023-09-22 18:38:34 +0200

    for loop

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log('loaded')
+
+let rootElement = document.querySelector('#root');
+/* console.log(rootElement) */
+
+/* let htmlContent = "<h2>Hello world</h2>"
+
+rootElement.insertAdjacentHTML('beforeend', htmlContent)
+ */
+/* let counter = 0;
+
+while (counter < 20) {
+    rootElement.insertAdjacentHTML('beforeend', htmlContent);
+    counter++; } /* = counter + 1 */
+
+/* let array = ["alma", "körte", "szilva", "eper", "birs", "vegyes"]
+
+for (let index = 0; index < array.lenght; index++) {
+    rootElement.insertAdjacentHTML('beforeend', `<h2> ${array[index]} </h2>`);
+} */
+
+
